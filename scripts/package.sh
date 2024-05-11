0668bc5041268d2d4a943f5c25391bbfa020c9a7
pfeairheller <pfeairheller@gmail.com>
2024-05-11 14:25:37 -0700

    Try using Python script with boto3 for uploads

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,4 +2,4 @@
 
 python setup.py sdist
 tar czf kourier-install-0.0.1.tar.gz dist/kourier-0.0.1.tar.gz conf/kourier.conf scripts/kourier.sh
-s3cmd put kourier-install-0.0.1.tar.gz s3://healthkeri-deployment/kourier/
+#s3cmd put kourier-install-0.0.1.tar.gz s3://healthkeri-deployment/kourier/
